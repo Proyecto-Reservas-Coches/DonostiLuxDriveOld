ccd147747aeb08dd35029d091ae7d6d774ffb38c
issam <is.elouafa@aulanz.net>
2023-02-07 12:25:42 +0100

    added InicioController and InicioApp

--- src/main/java/com/example/donostiluxdrive/InicioApplication.java
+++ src/main/java/com/example/donostiluxdrive/InicioApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class InicioApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(InicioApplication.class.getResource("Inicio-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Inicio");
         stage.setScene(scene);
         stage.show();
     }
