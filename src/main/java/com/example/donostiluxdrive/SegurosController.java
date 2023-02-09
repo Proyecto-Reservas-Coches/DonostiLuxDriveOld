a2b8ffe07345f505dff8d56cb167585211e1d10c
Dan159159 <ju.azurduy@aulanz.net>
2023-02-09 13:07:36 +0100

    Midificaciones en view de Seguros, InfoCoche y coches y CREACION de Controller de Seguros y InfoCoche

---
+++ src/main/java/com/example/donostiluxdrive/SegurosController.java
@@ -0,0 +1,33 @@
+package com.example.donostiluxdrive;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SegurosController {
+    @FXML
+    private Button InicioButtonNav;
+
+    @FXML
+    private Button CochesButtonNav;
+
+    @FXML
+    private Button SegurosButtonNav;
+
+    @FXML
+    private Button SignInButton;
+
+    @FXML
+    void goToInicio(ActionEvent event) {
+    }
+    @FXML
+    void goToCoches(ActionEvent event) {
+    }
+    @FXML
+    void goToSeguros(ActionEvent event) {}
+    @FXML
+    void goToSignIn(ActionEvent event) {
+
+    }
+
+}
